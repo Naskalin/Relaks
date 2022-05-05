958eede3210723cf4a72aadc6d6d04fe9f61e49f
Dreamer <miqpaa@gmail.com>
2022-05-06 00:17:41 +0300

    DateTime always utc

--- App/DbConfigurations/AppDbContext.cs
+++ App/DbConfigurations/AppDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using App.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace App.DbConfigurations;
 
@@ -41,15 +42,37 @@ public class AppDbContext : DbContext
         modelBuilder.ApplyConfiguration(new EntryInfoFtsConfiguration());
         
         modelBuilder.ApplyConfiguration(new FileConfiguration()); 
+        
+        // DateTime Always UTC
+        // https://stackoverflow.com/a/61243301/5638975
+        // https://github.com/dotnet/efcore/issues/4711#issuecomment-589842988
+        var dateTimeConverter = new ValueConverter<DateTime, DateTime>(
+            v => v.ToUniversalTime(),
+            v => DateTime.SpecifyKind(v, DateTimeKind.Utc));
 
-        // FTS
-        // modelBuilder.Entity<PostFts>(builder =>
-        // {
-        //     builder.HasKey(fts => fts.RowId);
-        //
-        //     builder
-        //         .Property(fts => fts.Match)
-        //         .HasColumnName(nameof(PostFts));
-        // });
+        var nullableDateTimeConverter = new ValueConverter<DateTime?, DateTime?>(
+            v => v.HasValue ? v.Value.ToUniversalTime() : v,
+            v => v.HasValue ? DateTime.SpecifyKind(v.Value, DateTimeKind.Utc) : v);
+
+        foreach (var entityType in modelBuilder.Model.GetEntityTypes())
+        {
+            if (entityType.IsKeyless)
+            {
+                continue;
+            }
+
+            foreach (var property in entityType.GetProperties())
+            {
+                if (property.ClrType == typeof(DateTime))
+                {
+                    property.SetValueConverter(dateTimeConverter);
+                }
+                else if (property.ClrType == typeof(DateTime?))
+                {
+                    property.SetValueConverter(nullableDateTimeConverter);
+                }
+            }
+        }
+        // -- DateTime Always UTC
     }
 }
