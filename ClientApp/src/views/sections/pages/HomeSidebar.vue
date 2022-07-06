5ed9f03ae103e36d21be990840ebbdda194fb1ae
Dreamer <miqpaa@gmail.com>
2022-07-06 04:28:56 +0300

    описание home

--- ClientApp/src/views/sections/pages/HomeSidebar.vue
+++ ClientApp/src/views/sections/pages/HomeSidebar.vue
@@ -3,9 +3,19 @@
         <q-card-section>
             <h5 class="q-mb-lg">Информация</h5>
             <ul>
-                <li>Предложения, ошибки, вопросы, отзывы в <a href="https://github.com/Naskalin/Relaks/discussions" target="_blank">Github</a></li>
-                <li>Оповещения о выходе новой версии в <a href="https://t.me/relaks_news" target="_blank">Telegram</a>.</li>
-                <li>Программа бесплатна, поэтому не стесняйтесь <a href="https://sobe.ru/na/na_razrabotku_relaks" target="_blank">поддержать проект рублём</a>.</li>
+                <li>Ошибки - <a href="https://github.com/Naskalin/Relaks/issues" target="_blank">максимально подробно
+                    проблему здесь</a>. Скриншоты, как воспроизвести проблему, операцинная система, версия программы
+                    предоставьте как можно больше данных
+                </li>
+                <li>Предложения, ошибки, вопросы, отзывы в <a href="https://github.com/Naskalin/Relaks/discussions"
+                                                              target="_blank">Github</a></li>
+                <li>Оповещения о новых версиях, о возможных проблемах с программой, подписывайтесь в <a
+                    href="https://t.me/relaks_news" target="_blank">Telegram</a> (пишу только по делу, обещаю не
+                    спамить)
+                </li>
+                <li>Программа бесплатна, не стесняйтесь <a href="https://boosty.to/relaks" target="_blank">поддержать
+                    проект рублём на Boosty</a>.
+                </li>
             </ul>
         </q-card-section>
     </q-card>
