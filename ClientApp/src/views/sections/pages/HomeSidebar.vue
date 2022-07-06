21b67ea8301abc3a034b720c07bed216b601c8e8
Dreamer <miqpaa@gmail.com>
2022-07-06 03:44:27 +0300

    telegram

--- ClientApp/src/views/sections/pages/HomeSidebar.vue
+++ ClientApp/src/views/sections/pages/HomeSidebar.vue
@@ -4,7 +4,7 @@
             <h5 class="q-mb-lg">Информация</h5>
             <ul>
                 <li>Предложения, ошибки, вопросы, отзывы в <a href="https://github.com/Naskalin/Relaks/discussions" target="_blank">Github</a></li>
-                <li>Оповещения о выходе новой версии в <a href="https://t.me/+s3jKRAu6YqJiMzUy" target="_blank">Telegram</a>.</li>
+                <li>Оповещения о выходе новой версии в <a href="https://t.me/relaks_news" target="_blank">Telegram</a>.</li>
                 <li>Программа бесплатна, поэтому не стесняйтесь <a href="https://sobe.ru/na/na_razrabotku_relaks" target="_blank">поддержать проект рублём</a>.</li>
             </ul>
         </q-card-section>
