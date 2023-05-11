4e9b714f2e55b743bc4f64da1fd9702c8b10a0fa
Dreamer <miqpaa@gmail.com>
2023-05-11 15:58:59 +0300

    получения контекста для миграций

---
+++ Relaks/src/Database/AppDbContextFactory.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Relaks.Managers;
+
+namespace Relaks.Database;
+
+/// <summary>
+/// Используется в качестве способа получения контекста базы данных при миграциях
+/// https://learn.microsoft.com/en-us/ef/core/cli/dbcontext-creation?tabs=dotnet-core-cli#from-a-design-time-factory
+/// </summary>
+public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+{
+    public AppDbContext CreateDbContext(string[] args)
+    {
+        var projectDir = AppDomain.CurrentDomain.BaseDirectory;
+        var relaksConfig = RelaksConfigManager.GetOrCreateConfig(projectDir);
+        
+        var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+        optionsBuilder.UseSqlite(relaksConfig.SqliteConnectionString());
+
+        return new AppDbContext(optionsBuilder.Options);
+    }
+}
