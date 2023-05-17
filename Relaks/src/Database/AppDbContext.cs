d66cfbdcbec4393e5865f74d92877f0e2e96796a
Dreamer <miqpaa@gmail.com>
2023-05-17 18:02:04 +0300

    utc comment

--- Relaks/src/Database/AppDbContext.cs
+++ Relaks/src/Database/AppDbContext.cs
@@ -39,33 +39,33 @@ public sealed class AppDbContext : DbContext
         // DateTime Always UTC
         // https://stackoverflow.com/a/61243301/5638975
         // https://github.com/dotnet/efcore/issues/4711#issuecomment-589842988
-        var dateTimeConverter = new ValueConverter<DateTime, DateTime>(
-            v => v.ToUniversalTime(),
-            v => DateTime.SpecifyKind(v, DateTimeKind.Utc));
-        
-        var nullableDateTimeConverter = new ValueConverter<DateTime?, DateTime?>(
-            v => v.HasValue ? v.Value.ToUniversalTime() : v,
-            v => v.HasValue ? DateTime.SpecifyKind(v.Value, DateTimeKind.Utc) : v);
-        
-        foreach (var entityType in modelBuilder.Model.GetEntityTypes())
-        {
-            if (entityType.IsKeyless)
-            {
-                continue;
-            }
-        
-            foreach (var property in entityType.GetProperties())
-            {
-                if (property.ClrType == typeof(DateTime))
-                {
-                    property.SetValueConverter(dateTimeConverter);
-                }
-                else if (property.ClrType == typeof(DateTime?))
-                {
-                    property.SetValueConverter(nullableDateTimeConverter);
-                }
-            }
-        }
+        // var dateTimeConverter = new ValueConverter<DateTime, DateTime>(
+        //     v => v.ToUniversalTime(),
+        //     v => DateTime.SpecifyKind(v, DateTimeKind.Utc));
+        //
+        // var nullableDateTimeConverter = new ValueConverter<DateTime?, DateTime?>(
+        //     v => v.HasValue ? v.Value.ToUniversalTime() : v,
+        //     v => v.HasValue ? DateTime.SpecifyKind(v.Value, DateTimeKind.Utc) : v);
+        //
+        // foreach (var entityType in modelBuilder.Model.GetEntityTypes())
+        // {
+        //     if (entityType.IsKeyless)
+        //     {
+        //         continue;
+        //     }
+        //
+        //     foreach (var property in entityType.GetProperties())
+        //     {
+        //         if (property.ClrType == typeof(DateTime))
+        //         {
+        //             property.SetValueConverter(dateTimeConverter);
+        //         }
+        //         else if (property.ClrType == typeof(DateTime?))
+        //         {
+        //             property.SetValueConverter(nullableDateTimeConverter);
+        //         }
+        //     }
+        // }
         
     }
 }
