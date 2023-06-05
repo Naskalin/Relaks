8e7356f8d5c2889897ec2aebf44a58139ae3da50
Dreamer <miqpaa@gmail.com>
2023-06-05 17:30:26 +0300

    sortable data interop

--- Relaks/assets/src/js/_dataset_sortable.js
+++ Relaks/assets/src/js/_dataset_sortable.js
@@ -1,37 +1,24 @@
 ﻿import Sortable from 'sortablejs';
 
-// var el = document.getElementById('js-sortable');
-// var sortable = Sortable.create('js-sortable');
+window.SortableJsGroupsInit = async (netDataset) => {
+    console.log(netDataset);
+    const groupWrapperEl = document.getElementById('js-sortable-groups');
+    if (!groupWrapperEl) throw new Error(`Sortable group element not found`);
+    const sortableGroups = Sortable.create(groupWrapperEl, {
+        handle: '.js-sortable-handle',
+        onSort: async function () {
+            await netDataset.invokeMethodAsync('OnSortGroups', sortableGroups.toArray())
+        }
+    });
+};
 
-window.SortableJsInit = (data) => {
-    console.log(data['elementId'])
-    const el = document.getElementById(data['elementId']);
-    if (!el) throw new Error(`Sortable element not found`);
-    window.SortableJs = Sortable.create(el);
-
-    // const imgId = 'img-' + data['blockId'];
-    // let imgEl = document.getElementById(imgId);
-    // if (!imgEl) {
-    //     imgEl = document.createElement('img');
-    //     imgEl.setAttribute('id', imgId);
-    //     blockEl.appendChild(imgEl);
-    // }
-    // imgEl.setAttribute('src', data['imgSrc']);
-    //
-    // // show preview
-    // const previewEl = document.getElementById(`preview-${data['blockId']}`);
-    // if (previewEl) {
-    //     previewEl.style.cssText = 'overflow: hidden;width: 150px; height: 150px;display: block;border: 1px solid var(--bs-border-color);'
-    // }
-    //
-    // if (window.CropperJs) {
-    //     window.CropperJs.destroy();
-    // }
-    //
-    // window.CropperJs = new Cropper(imgEl, {
-    //     viewMode: 1,
-    //     dragMode: 'move',
-    //     aspectRatio: 1,
-    //     preview: previewEl,
-    // })
+window.SortableJsItemsInit = async (netDatasetGroup, itemsContainerId) => {
+    const itemsContainerEl = document.getElementById(itemsContainerId);
+    if (!itemsContainerEl) throw new Error(`Sortable items element not found`);
+    const sortableItems = Sortable.create(itemsContainerEl, {
+        handle: '.js-sortable-handle',
+        onSort: async function () {
+            await netDatasetGroup.invokeMethodAsync('OnSortItems', sortableItems.toArray())
+        }
+    });
 };
