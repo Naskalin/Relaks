81dae00868d4a7325fa7e9e9ae9cf334de8121ad
Dreamer <miqpaa@gmail.com>
2023-08-16 17:49:01 +0300

    // удаляем ID владельца из связей в файле с дрегими объединениями (если выбрали по ошибке)

---
+++ Relaks/src/Models/ObjectiveModels/ObjectivePerformer.cs
@@ -0,0 +1,9 @@
+﻿// namespace Relaks.Models.ObjectiveModels;
+//
+// public class ObjectivePerformer
+// {
+//     public Guid Id { get; set; }
+//     public BaseEntry Entry { get; set; } = null!;
+//     public Objective Objective { get; set; } = null!;
+//     public DateTime 
+// }
