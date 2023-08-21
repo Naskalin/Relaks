8e08ac30c375a4b8ada33ec01e1e5467a4ac4419
Dreamer <miqpaa@gmail.com>
2023-08-21 13:09:19 +0300

    structures migration

--- Relaks/Migrations/20230821100844_CreateStructures.Designer.cs
+++ Relaks/Migrations/20230821100844_CreateStructures.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Relaks.Database;
 
@@ -10,9 +11,11 @@ using Relaks.Database;
 namespace Relaks.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230821100844_CreateStructures")]
+    partial class CreateStructures
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
