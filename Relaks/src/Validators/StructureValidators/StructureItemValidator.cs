4f91169e71cabbb0bbc85f1bcd1ae08457092bb6
Dreamer <miqpaa@gmail.com>
2023-08-25 13:24:47 +0300

    sync group lifetime

---
+++ Relaks/src/Validators/StructureValidators/StructureItemValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using Relaks.Models.StructureModels;
+
+namespace Relaks.Validators.StructureValidators;
+
+public class StructureItemValidator : AbstractValidator<StructureItem>
+{
+    public StructureItemValidator()
+    {
+        RuleFor(x => x.Title).MaximumLength(150);
+        RuleFor(x => x.Description).MaximumLength(300);
+        RuleFor(x => x.EntryId).NotEmpty();
+        RuleFor(x => x.GroupId).NotEmpty();
+    }
+}
