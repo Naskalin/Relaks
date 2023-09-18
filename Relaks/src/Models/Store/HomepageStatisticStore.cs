889f84f3b5a37c874e83c3e0f6921802f36b0d2f
Dreamer <miqpaa@gmail.com>
2023-09-18 17:45:44 +0300

    комментарий, если нет связей

---
+++ Relaks/src/Models/Store/HomepageStatisticStore.cs
@@ -0,0 +1,34 @@
+﻿using Relaks.Database;
+
+namespace Relaks.Models.Store;
+
+public class HomepageStatisticStore
+{
+    private readonly AppDbContext _db;
+
+    public HomepageStatisticStore(AppDbContext db)
+    {
+        _db = db;
+    }
+
+    public Dictionary<string, int> EntryCounts { get; set; } = new();
+    public Dictionary<string, int> FileCounts { get; set; } = new();
+    public Dictionary<string, int> EInfoCounts { get; set; } = new();
+
+    public void FindData()
+    {
+        EntryCounts["Компании"] = _db.ECompanies.Count();
+        EntryCounts["Встречи"] = _db.EMeets.Count();
+        EntryCounts["Люди"] = _db.EPersons.Count();
+        
+        FileCounts["Файлы"] = _db.EntryFiles.Count();
+        FileCounts["Метки файлов"] = _db.EntryFileTags.Count();
+        FileCounts["Категории файлов"] = _db.EntryFileCategories.Count();
+        
+        EInfoCounts["Телефоны"] = _db.EiPhones.Count();
+        EInfoCounts["Даты"] = _db.EiDates.Count();
+        EInfoCounts["Эл. почты"] = _db.EiEmails.Count();
+        EInfoCounts["Ссылки"] = _db.EiUrls.Count();
+        EInfoCounts["Наборы данных"] = _db.EiDatasets.Count();
+    }
+}
