e36752fe77bef4e59b782a2b81196da7101a319c
Naskalin <Naskalin@users.noreply.github.com>
2023-10-26 00:46:31 +0300

    Обнаружение рекурсии по treePath

--- Relaks/src/Validators/TreeValidator.cs
+++ Relaks/src/Validators/TreeValidator.cs
@@ -1,4 +1,5 @@
 ﻿using FluentValidation;
+using LinqKit;
 using Microsoft.EntityFrameworkCore;
 using Relaks.Database;
 using Relaks.Interfaces;
@@ -22,21 +23,36 @@ public class TreeValidator<TEntity> : AbstractValidator<TEntity> where TEntity :
         // Нет родителя = нет рекурсии
         if (!node.ParentId.HasValue) return false;
         
-        var existNode = q.FirstOrDefault(x => x.Id.Equals(node.Id));
-        // Если ещё не в бд, то рекурсии не будет
-        if (existNode == null) return false;
-        if (existNode.Id.Equals(existNode.ParentId)) return true;
-
-        // flat tree
-        var tree = q
-            .Where(x => x.TreePath.Contains(existNode.Id.ToString()))
-            .Include(x => x.Children)
-            .ToList();
-
-        // normalize tree
-        var nodeWithChildren = tree.First(x => x.Id.Equals(node.Id));
+        // Id == parentId = рекурсия
+        if (node.Id.Equals(node.ParentId)) return true;
         
-        var child = TreeManager.FindChildById(nodeWithChildren, node.ParentId.Value);
-        return child != null;
+        // Нода ещё не в бд, то рекурсии не будет
+        var existNode = q.FirstOrDefault(x => x.Id.Equals(node.Id));
+        if (existNode == null) return false;
+        
+        var parent = q.FirstOrDefault(x => x.Id.Equals(node.ParentId));
+        ArgumentNullException.ThrowIfNull(parent);
+
+        // Node.Parent
+        // -- Node.Children
+        // ---- Node.SubChildren
+        // Предполагаем, что Node.Parent пытается выбрать в качестве родителя Node.Children или Node.SubChildren
+        // Нужно проверить, есть ли в пути выбираемой в качестве родителя ноды Id Node.Parent, то есть Id текущей ноды
+        return parent
+            .TreePath.Split("/")
+            .Where(x => !string.IsNullOrEmpty(x))
+            .Any(x => Guid.Parse(x).Equals(node.Id));
+        
+        // // flat tree
+        // var tree = q
+        //     .Where(x => x.TreePath.Contains(node.Id.ToString()))
+        //     .Include(x => x.Children)
+        //     .ToList();
+        //
+        // // normalize tree
+        // var nodeWithChildren = tree.First(x => x.Id.Equals(node.Id));
+        //
+        // var child = TreeManager.FindChildById(nodeWithChildren, node.ParentId.Value);
+        // return child != null;
     }
 }
