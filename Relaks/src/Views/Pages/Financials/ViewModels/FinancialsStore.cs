0f0da89ad0a554b07190726fabcd1c5d6edf2a53
Naskalin <Naskalin@users.noreply.github.com>
2023-12-12 15:18:44 +0300

    tree items component

--- Relaks/src/Views/Pages/Financials/ViewModels/FinancialsStore.cs
+++ Relaks/src/Views/Pages/Financials/ViewModels/FinancialsStore.cs
@@ -1,11 +1,7 @@
 ﻿using BootstrapBlazor.Components;
-using Microsoft.EntityFrameworkCore;
 using Relaks.Database;
 using Relaks.Database.Repositories;
-using Relaks.Interfaces;
-using Relaks.Managers;
 using Relaks.Models.FinancialModels;
-using Relaks.Models.Misc;
 
 namespace Relaks.Views.Pages.Financials.ViewModels;
 
@@ -29,7 +25,7 @@ public class FinancialsStore(AppDbContext db)
         EditTransaction,
     }
 
-    public BodyEnum BodyState { get; set; } = BodyEnum.AddTransaction;
+    public BodyEnum BodyState { get; set; } = BodyEnum.Default;
     public SidebarEnum SidebarState { get; set; } = SidebarEnum.Default;
     public Guid? BodyEditTransactionId { get; set; }
     public Guid? BodyEditTransactionCategoryId { get; set; }
@@ -37,34 +33,13 @@ public class FinancialsStore(AppDbContext db)
     public Guid? SidebarEditAccountId { get; set; }
     public List<FinancialTransactionCategory> TransactionCategories { get; set; } = new();
     public List<FinancialCurrency> Currencies { get; set; } = new();
-    // private List<ITree<FinancialTransactionCategory>> TransactionCategories { get; set; } = new();
-    // public PaginatableResult<FinancialTransaction> Transactions { get; set; } = new();
     public Guid? AccountId { get; set; }
-    // public FinancialTransactionListRequest TransactionListRequest { get; set; } = new() {Page = 1, PerPage = 10};
 
     public void Initialize()
     {
         FindCurrencies();
         FindTransactionCategories();
-        // FindTransactions();
     }
-
-    // public void FindForAccount()
-    // {
-    //     FindTransactions();
-    // }
-
-    // public void FindTransactions()
-    // {
-    //     var q = db.FinancialTransactions
-    //         .Include(x => x.Entry)
-    //         .Include(x => x.Account).ThenInclude(a => a.FinancialCurrency)
-    //         .OrderByDescending(x => x.CreatedAt);
-    //     
-    //     Transactions = AccountId.HasValue 
-    //         ? q.Where(x => x.AccountId.Equals(AccountId.Value)).ToPaginatedResult(TransactionListRequest) 
-    //         : q.ToPaginatedResult(TransactionListRequest);
-    // }
 
     public void FindTransactionCategories()
     {
@@ -79,9 +54,6 @@ public class FinancialsStore(AppDbContext db)
             .ThenByDescending(x => x.Id.Equals("EUR"))
             .ToList();
     }
-
-    public List<SelectedItem> FindTransactionCategoriesSelectOptions() =>
-        TransactionCategories.ToTreeSelect().Select(x => new SelectedItem(x.Value.ToString(), x.Title)).ToList();
 
     public List<SelectedItem> AccountCategoriesSelectOptions() => db.FinancialAccountCategories
         .Select(x => new SelectedItem(x.Id.ToString(), x.Title))
